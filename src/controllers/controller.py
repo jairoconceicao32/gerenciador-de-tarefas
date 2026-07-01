45b129a65e8b556c85217a447dcfc81104281232
jairoconceicao32 <jairoconceicao689@gmail.comgit>
2026-07-01 19:35:16 -0300

    refactor: controller agora usa exceções em vez de retorno de tupla

--- src/controllers/controller.py
+++ src/controllers/controller.py
@@ -1,26 +1,28 @@
+from src.exceptions.errors import InvalidNameError, InvalidDescriptionError, InvalidDeadlineError, InvalidTaskidError, NotFoundError
+
 def create(name, description, manager):
     name = name.strip()
     description = description.strip()
     if len(name) < 4:
-        return False, "O nome deve conter pelo menos 4 caracteres."
+        raise InvalidNameError("O nome deve conter pelo menos 4 caracteres.")
     if len(description) < 5:
-        return False, "A descrição deve conter pelo menos 5 caracteres."
-    return True, manager.create_task(name, description)
+        raise InvalidDescriptionError("A descrição deve conter pelo menos 5 caracteres.")
+    return manager.create_task(name, description)
 
 def read(manager):
     tasks = manager.list_tasks()
     return tasks
 
+
 def delete(task_id, manager):
-    task_id= task_id.strip()
-    if task_id=="":
-        return False, "O ID da tarefa não pode ser vazio."
+    task_id = task_id.strip()
+    if task_id == "":
+        raise InvalidTaskidError("O ID da tarefa não pode ser vazio.")
     try:
         task_id = int(task_id)
     except ValueError:
-        return False, "Digite apenas números."
+        raise InvalidTaskidError("Digite apenas números.")
     result = manager.delete_task(task_id)
-    if result:
-        return True, "Tarefa deletada com sucesso."
-    else:
-        return False, "Tarefa não encontrada."
+    if not result:
+        raise NotFoundError("Tarefa não encontrada.")
+    return result
